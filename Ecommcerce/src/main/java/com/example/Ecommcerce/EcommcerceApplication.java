7f29fb9d934995d391d51e923ac927df39564bd7
Ashu19025 <Ashishkumar19025@gmail.com>
2025-03-10 23:02:36 +0530

    Initial commit

---
+++ Ecommcerce/src/main/java/com/example/Ecommcerce/EcommcerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecommcerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommcerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommcerceApplication.class, args);
+	}
+
+}
