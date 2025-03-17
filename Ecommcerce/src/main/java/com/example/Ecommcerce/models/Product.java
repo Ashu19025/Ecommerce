b8959cbe7b04a190df841f8c73cbe6fc1f75ad93
Ashu19025 <Ashishkumar19025@gmail.com>
2025-03-17 23:03:55 +0530

    N

--- Ecommcerce/src/main/java/com/example/Ecommcerce/models/Product.java
+++ Ecommcerce/src/main/java/com/example/Ecommcerce/models/Product.java
@@ -1,7 +1,10 @@
 package com.example.Ecommcerce.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -22,6 +25,37 @@ public class Product {
     @NotNull
     @Size(min = 3,max = 30,message = "product name size should be between 3-30")
     private String productName;
+
+    @NotNull
+    @DecimalMin(value = "0.00")
+    private Double price;
+
+    private String description;
+
+    @NotNull
+    private String manufacturer;
+
+    @NotNull
+    @Min(value = 0)
+    private Integer quantity;
+
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum category;
+
+    @Enumerated(EnumType.STRING)
+    private ProductStatus status;
+
+//	@ManyToMany(cascade = CascadeType.ALL)
+//	private Order order;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Seller seller;
+
+//	@ManyToMany
+//	@JsonIgnore
+//	private List<Cart> productCarts = new ArrayList<>();
+
 
 
 }
